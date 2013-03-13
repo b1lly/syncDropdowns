43a3e6af5862d15885a274870a1fbde4c8cdc46a
Billy Bastardi <billy@wabism.com>
2013-03-13 01:41:23 -0400

    minor syntax improvements and comment fixes

--- syncdropdowns.js
+++ syncdropdowns.js
@@ -1,53 +1,52 @@
 /**
  * jQuery Plugin to Sync multiple "select" elements together
  * by their option values
+ * 
  * @author billy@yext.com
  *  
  * @usage $('selector').syncDropdowns();
- * 
- * 
  */
 (function($) {
   $.fn.syncDropdowns = function() {
-    var selectedOptions = {}; // Used to store a history of dropdown selected options
-    var elms = this; // Collection of the dropdowns to sync
+    var selectedOptions = {}, // Used to store a history of selected options
+        $elms = this; // Collection of the dropdowns (elements) to sync
 	  
     // Sync only the selectors that this was initiated on
     return this.each(function(selectedIndex) {
       
       /**
-       * Update the options of all the dropdowns (exclude selected)
-       * based on the option that was selected
+       * Update the options of all the dropdowns 
+       * based on the option that was selected (exclude selected)
        * 
        * @param {Integer} selectedIndex
        * @param {String} selectedValue
        * @param {String} previousValue
        */
       var updateDropdowns = function(selectedIndex, selectedValue, previousValue) {
-        elms.each(function(index) {
+        $elms.each(function(index) {
           if (selectedIndex == index) {
             return;
           }          
           
-          // Add options back if it's no longer selected by another dropdown
+          // Add the option back if the changed dropdown had a previous selection
           if (previousValue) {
             $(this).find('option[value="' + previousValue + '"]').removeAttr('disabled');
           }
           
-          // Remove the selected option from other dropdowns     
+          // Remove the option that was selected from the changed dropdown     
           $(this).find('option[value="' + selectedValue + '"]').attr('disabled', 'disabled');
         });
       }  
 
       // Listen for the change of options and update
-      // the other dropdowns based on the selection
+      // the dropdowns based on the users selection
       $(this).change(function() {
         var selectedText = $(this).find(':selected').text(),
             selectedValue = $(this).val(),
             previousValue;
         
         // Check if the selected dropdown had a previous value
-        // and then refresh the other dropdowns accordingly
+        // and then update the other dropdowns accordingly
         if (selectedOptions[selectedIndex]) {
           previousValue = selectedOptions[selectedIndex].value;
         }        
@@ -58,7 +57,6 @@
           value : selectedValue,
           text : selectedText
         };
-
       });  
     });
   }
